b1b7ec35ea9748865bfdec84866dc0358ca74879
dashbun <swayam.235386101@vcet.edu.in>
2024-07-19 10:05:27 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,18 @@
+class FirstProgram  
+{
+     public static void main(String args[])
+
+    { 
+	int len=args.length;
+	System.out.println( "length of line="+len);
+	for(int i=0;i<len;i++)
+	{
+	System.out.println( args[i]);
+	}
+
+	
+    
+    }
+
+
+}
